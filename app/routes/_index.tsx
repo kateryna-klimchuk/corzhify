36c02adc414e47f9ddf56eaf81bd99c75795d34d
Kateryna Klimchuk <ketyclima@gmail.com>
2025-01-18 13:00:40 +0100

    Setup Tailwind css

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,14 +2,14 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Corzhify" },
+    { name: "description", content: "Welcome to Corzhify!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div className="text-orange-500">
       <h1>Welcome to Corzhify</h1>
     </div>
   );
---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} as Config;
+
+export default config;
